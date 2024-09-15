f028c30cc35725a219285f7fd7cd09cec2f22461
faisal1441 <160399431+faisal1441@users.noreply.github.com>
2024-09-15 17:43:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var toggleButton = document.getElementById('toggleSkills');
+var skillsSection = document.getElementById('skills');
+toggleButton.addEventListener('click', function () {
+    if (skillsSection.style.display === 'none') {
+        skillsSection.style.display = 'block';
+    }
+    else {
+        skillsSection.style.display = 'none';
+    }
+});
